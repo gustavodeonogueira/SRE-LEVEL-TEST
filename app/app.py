7feed244abacb28cdf4875e5b2230685360bc7c4
Gustavodeonogueira <gustavodeonogueira@DESKTOP-1MLM35U.>
2026-02-03 11:11:06 -0300

    fix: include request counter in home endpoint for tests

--- app/app.py
+++ app/app.py
@@ -21,6 +21,7 @@ def home():
     return jsonify({
         "message": "Aplicação SRE Nível 1",
         "version": os.getenv("APP_VERSION", "1.0.0")
+         "total_requests": metrics["total_requests"] 
     })
 
 @app.route('/health')
